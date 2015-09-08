baca26f9c68c01d96c7bcdc0a97fc0e3a3f4f78e
Thomas Foster <thomas.foster@noaa.gov>
2015-09-08 10:08:20 -0400

    added unless to fix idempotency

--- puppet/modules/vtlrepo/manifests/initial_sync.pp
+++ puppet/modules/vtlrepo/manifests/initial_sync.pp
@@ -3,6 +3,7 @@ class vtlrepo::initial_sync inherits vtlrepo {
   exec { 'Adding at command to run sync script':
     path      => '/bin:/usr/bin/',
     command   => "echo /root/initial_repo.sh |at now + 2 minute",
+    unless    => "at -c 1 |grep initial_repo.sh 2> /dev/null",
     creates   => "/root/.initialreposync",
   }
 }
