b61d3bf240cf1c1d0e192acee80fa95274c9ed8d
root <root@foreman.hq.nesdis.noaa.doc>
2015-09-11 10:04:58 -0400

    added new templates and modified params

--- puppet/modules/vtlrepo/manifests/params.pp
+++ puppet/modules/vtlrepo/manifests/params.pp
@@ -17,9 +17,79 @@ $centos6_epel    = { 'reponame'       => 'centos6_epel',
                      'gpgcheck'       => '0',
                    }
 
+$centos6_base    = { 'reponame'       => 'centos6_base',
+                     'name'           => 'This is a centos6 base repo',
+                     'baseurl'        => 'http://mirror.centos.org/centos/6/os/x86_64/',
+                     'enabled'        => '1',
+                     'gpgcheck'       => '0',
+                   }
+
+$centos6_extras  = { 'reponame'       => 'centos6_extras',
+                     'name'           => 'This is a centos6 extras repo',
+                     'baseurl'        => 'http://mirror.centos.org/centos/6/extras/x86_64/',
+                     'enabled'        => '1',
+                     'gpgcheck'       => '0',
+                   }
+
+$centos6_puppet = { 'reponame'        => 'centos6_puppet',
+                     'name'           => 'This is a centos6 puppet repo',
+                     'baseurl'        => 'http://yum.puppetlabs.com/el/6/products/x86_64/'
+                     'enabled'        => '1',
+                     'gpgcheck'       => '0',
+                   }
+
+$centos6_puppet_deps = { 'reponame'   => 'centos6_puppet_deps',
+                     'name'           => 'This is a centos6 puppet deps repo',
+                     'baseurl'        => 'http://mirror.centos.org/centos/6/extras/x86_64/',
+                     'enabled'        => '1',
+                     'gpgcheck'       => '0',
+                   }
+
+$centos6_updates = { 'reponame'   => 'centos6_updates',
+                     'name'           => 'This is a centos6 updates repo',
+                     'baseurl'        => 'http://mirror.centos.org/centos/6/updates/x86_64/',
+                     'enabled'        => '1',
+                     'gpgcheck'       => '0',
+                   }
+
 $centos7_puppet  = { 'reponame'       => 'centos7_puppetlabs',
                      'name'           => 'This is a centos7 puppetlabs repo',
                      'baseurl'        => 'http://yum.puppetlabs.com/el/7/products/x86_64/',
+                     'enabled'        => '1',
+                     'gpgcheck'       => '0',
+                   }
+
+$centos7_puppet_deps  = { 'reponame'  => 'centos7_puppetlabs_deps',
+                     'name'           => 'This is a centos7 puppetlabs deps repo',
+                     'baseurl'        => 'http://yum.puppetlabs.com/el/7/dependencies/x86_64/',
+                     'enabled'        => '1',
+                     'gpgcheck'       => '0',
+                   }
+
+$centos7_base    = { 'reponame'       => 'centos7_base',
+                     'name'           => 'This is a centos7 base repo',
+                     'baseurl'        => 'http://mirror.centos.org/centos/7/os/x86_64/',
+                     'enabled'        => '1',
+                     'gpgcheck'       => '0',
+                   }
+
+$centos7_epel    = { 'reponame'       => 'centos7_epel',
+                     'name'           => 'This is a centos7 epel repo',
+                     'baseurl'        => 'http://dl.fedoraproject.org/pub/epel/7/x86_64/',
+                     'enabled'        => '1',
+                     'gpgcheck'       => '0',
+                   }
+
+$centos7_extras  = { 'reponame'       => 'centos7_extras',
+                     'name'           => 'This is a centos7 extras repo',
+                     'baseurl'        => 'http://mirror.centos.org/centos/7/extras/x86_64/',
+                     'enabled'        => '1',
+                     'gpgcheck'       => '0',
+                   }
+
+$centos7_updates = { 'reponame'       => 'centos7_updates',
+                     'name'           => 'This is a centos7 updates repo',
+                     'baseurl'        => 'http://mirror.centos.org/centos/7/updates/x86_64/',
                      'enabled'        => '1',
                      'gpgcheck'       => '0',
                    }
@@ -64,10 +134,40 @@ $repos = { 'glusterfs-epel'   => {
                                    'content'        => template('vtlrepo/glusterfs-epel.erb'),
                                 },
 
+           'centos6-base'     => { 
+                                   'ensure'         => present,
+                                   'path'           => '/etc/rh6.repos.d/centos6-base.repo',
+                                   'content'        => template('vtlrepo/centos6_base.erb'), 
+                                },
+
            'centos6-epel'     => { 
                                    'ensure'         => present,
                                    'path'           => '/etc/rh6.repos.d/centos6-epel.repo',
                                    'content'        => template('vtlrepo/centos6_epel.erb'), 
+                                },
+
+           'centos6-extras'   => { 
+                                   'ensure'         => present,
+                                   'path'           => '/etc/rh6.repos.d/centos6-extras.repo',
+                                   'content'        => template('vtlrepo/centos6_extras.erb'), 
+                                },
+
+           'centos6-puppet'   => { 
+                                   'ensure'         => present,
+                                   'path'           => '/etc/rh6.repos.d/centos6-puppet.repo',
+                                   'content'        => template('vtlrepo/centos6_puppet.erb'), 
+                                },
+
+           'centos6-puppet_deps'   => { 
+                                   'ensure'         => present,
+                                   'path'           => '/etc/rh6.repos.d/centos6-puppet-deps.repo',
+                                   'content'        => template('vtlrepo/centos6_puppet_deps.erb'), 
+                                },
+
+           'centos6-updates'   => { 
+                                   'ensure'         => present,
+                                   'path'           => '/etc/rh6.repos.d/centos6-updates.repo',
+                                   'content'        => template('vtlrepo/centos6_updates.erb'), 
                                 },
 
            'centos7-puppet'   => {
@@ -76,8 +176,37 @@ $repos = { 'glusterfs-epel'   => {
                                    'content'        => template('vtlrepo/centos7_puppet.erb'),
                                 },
 
-    }
+           'centos7-puppet-deps'   => {
+                                   'ensure'         => present,
+                                   'path'           => '/etc/rh7.repos.d/centos7-puppet-deps.repo',
+                                   'content'        => template('vtlrepo/centos7_puppet_deps.erb'),
+                                },
 
+           'centos7-base'   => {
+                                   'ensure'         => present,
+                                   'path'           => '/etc/rh7.repos.d/centos7-base.repo',
+                                   'content'        => template('vtlrepo/centos7_base.erb'),
+                                },
+
+           'centos7-epel'   => {
+                                   'ensure'         => present,
+                                   'path'           => '/etc/rh7.repos.d/centos7-epel.repo',
+                                   'content'        => template('vtlrepo/centos7_epel.erb'),
+                                },
+
+           'centos7-extras' => {
+                                   'ensure'         => present,
+                                   'path'           => '/etc/rh7.repos.d/centos7-extras.repo',
+                                   'content'        => template('vtlrepo/centos7_extras.erb'),
+                                },
+
+           'centos7-updates' => {
+                                   'ensure'         => present,
+                                   'path'           => '/etc/rh7.repos.d/centos7-updates.repo',
+                                   'content'        => template('vtlrepo/centos7_updates.erb'),
+                                },
+
+    }
 
 $myconfigs = { 'rh6.conf'     => { 
                                    'ensure'         => present,
